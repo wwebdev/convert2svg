dc4468ee34c5a327fac3217cf7ac0543e9489d03
Vincenius <vincet.will@vaitrade.de>
2020-01-02 07:57:23 +0100

    rename footer component

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as S from './styled'
 
-const Test = props => {
+const Footer = props => {
     const d = new Date();
     const year = d.getFullYear();
     return (
@@ -18,4 +18,4 @@ const Test = props => {
     )
 }
 
-export default Test
+export default Footer
